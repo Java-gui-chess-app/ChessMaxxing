fb77ba41ef9f406865f66556f042d10b5372cf68
Delilah Adams <159796184+delilahadams@users.noreply.github.com>
2024-05-06 17:22:33 -0400

    modified:   Chessmaxing/src/Engine.java 	new file:   Chessmaxing/src/Knight.java

--- Chessmaxing/src/Engine.java
+++ Chessmaxing/src/Engine.java
@@ -10,17 +10,17 @@ public class Engine extends Chess {
         if (turnNumber % 2 == 0) isWhite = true;
         else isWhite = false;
         //checks the peice being moved and calls on that peice's function or method or whatever
-        /*if(board[(move2[0]) - 1][move2[1]-1] == '♔' || board[(move2[0]) - 1][(move2[1])-1] == '♚')
+        /*if(board[move2[0]][move2[1]] == '♔' || board[move2[0]][move2[1]] == '♚')
         {board = King.main(move2, board);}
-        else if(board[(move2[0]) - 1][(move2[1])-1] == '♕' || board[(move2[0]) - 1][(move2[1])-1] == '♛')
+        else if(board[move2[0]][move2[1]] == '♕' || board[move2[0]][move2[1]] == '♛')
         {board = Queen.main(move2, board);}
         else */if(board[(move2[0])][(move2[1])] == '♖' || board[(move2[0])][(move2[1])] == '♜')
         {board = Rook.main(move2, board, isWhite);}/*
-        else if(board[(move2[0]) - 1][(move2[1])-1] == '♗' || board[(move2[0]) - 1][(move2[1])-1] == '♝')
-        {board = Bishop.main(move2, board);}
-        else if(board[(move2[0]) - 1][(move2[1])-1] == '♘' || board[(move2[0]) - 1][(move2[1])-1] == '♞')
-        {board = Knight.main(move2, board);}
-        */else if(board[(move2[0])][(move2[1])] == '♙' || board[(move2[0])][(move2[1])] == '♟')
+        else if(board[move2[0]][move2[1]] == '♗' || board[move2[0]][move2[1]] == '♝')
+        {board = Bishop.main(move2, board);}*/
+        else if(board[move2[0]][move2[1]] == '♘' || board[move2[0]][move2[1]] == '♞')
+        {board = Knight.main(move2, board, isWhite);}
+        else if(board[(move2[0])][(move2[1])] == '♙' || board[(move2[0])][(move2[1])] == '♟')
         {board = Pawn.main(move2, board, isWhite);}
         else {JOptionPane.showMessageDialog(null, "the peice you attempted to move is not a valid peice", "Oops!", 0); };
         return board;
