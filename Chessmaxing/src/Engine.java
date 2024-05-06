7b28d87537a10d8ff92fbc2e64f720d9e38d4294
Delilah Adams <159796184+delilahadams@users.noreply.github.com>
2024-05-06 16:14:55 -0400

    modified:   Chessmaxing/src/Chess.java 	modified:   Chessmaxing/src/Engine.java 	modified:   Chessmaxing/src/Pawn.java     modified:   Chessmaxing/src/Rook.java

--- Chessmaxing/src/Engine.java
+++ Chessmaxing/src/Engine.java
@@ -7,8 +7,8 @@ public class Engine extends Chess {
         String move1[] = move.split(" ", 4);
         int move2[] = {Integer.parseInt(move1[0])-1, Integer.parseInt(move1[1])-1, Integer.parseInt(move1[2])-1, Integer.parseInt(move1[3])-1};
         //checks who's turn it is
-        if (turnNumber % 2 == 0) {isWhite = true;}
-        else {isWhite = false;}
+        if (turnNumber % 2 == 0) isWhite = true;
+        else isWhite = false;
         //checks the peice being moved and calls on that peice's function or method or whatever
         /*if(board[(move2[0]) - 1][move2[1]-1] == '♔' || board[(move2[0]) - 1][(move2[1])-1] == '♚')
         {board = King.main(move2, board);}
--- Chessmaxing/src/Pawn.java
+++ Chessmaxing/src/Pawn.java
@@ -1,19 +1,19 @@
 import javax.swing.JOptionPane;
 public class Pawn extends Chess {
-    public static Character[][] main(int[] move2, Character[][] board, Boolean isWhite) {
+    public static Character[][] main(int[] move2, Character[][] board, boolean isWhite) {
         {
         //checks if peice being moved is correct colour
-        if ((isWhite == false && board[move2[0]][move2[1]] == '♟' || isWhite == true && board[move2[0]][move2[1]] == '♙' ) == false) {JOptionPane.showMessageDialog(null, "That is not your peice.", "Oops!", 0); return board;}
+        if (((isWhite == false && board[move2[0]][move2[1]] == '♟') || (isWhite == true && board[move2[0]][move2[1]] == '♙')) == false) {JOptionPane.showMessageDialog(null, "That is not your peice.", "Oops!", 0); return board;};
         //checks if pawn can move to landing space
         if (isWhite == true){
-            if (move2[0] - move2[2] == 1 && move2[1] == move2[3]);
-            else if ((move2[0]-move2[2] == 2 && move2[0] == 6) && move2[1] == move2[3]);
+            if ((move2[0] - move2[2] == 1 && move2[1] == move2[3]) && board[move2[2]][move2[3]] == ' ');
+            else if (((move2[0]-move2[2] == 2 && move2[0] == 6) && move2[1] == move2[3]) && (board[move2[2]+1][move2[3]] == ' ' && board[move2[2]][move2[3]] == ' '));
             else if ((move2[0] - move2[2] == 1 && Math.abs(move2[1] - move2[3]) == 1) && (board[move2[2]][move2[3]] == '♖' || board[move2[2]][move2[3]] == '♘' || board[move2[2]][move2[3]] == '♗' || board[move2[2]][move2[3]] == '♔' || board[move2[2]][move2[3]] == '♕' || board[move2[2]][move2[3]] == '♙'));
             else {JOptionPane.showMessageDialog(null, "That peice can not move to that square", "Oops!", 0); return board;};
             }
         else {
-            if (move2[0] - move2[2] == -1 && move2[1] == move2[3]);
-            else if ((move2[0]-move2[2] == -2 && move2[0] == 1) && move2[1] == move2[3]);
+            if ((move2[0] - move2[2] == -1 && move2[1] == move2[3]) && board[move2[2]][move2[3]] == ' ');
+            else if (((move2[0]-move2[2] == -2 && move2[0] == 1) && move2[1] == move2[3]) && (board[move2[2]-1][move2[3]] == ' ' && board[move2[2]][move2[3]] == ' '));
             else if ((move2[0] - move2[2] == -1 && Math.abs(move2[1] - move2[3]) == 1) && board[move2[2]][move2[3]] == '♜' || board[move2[2]][move2[3]] == '♞' || board[move2[2]][move2[3]] == '♝' || board[move2[2]][move2[3]] == '♚' || board[move2[2]][move2[3]] == '♛' || board[move2[2]][move2[3]] == '♟');
             else {JOptionPane.showMessageDialog(null, "That peice can not move to that square", "Oops!", 0); return board;};
             };
@@ -25,8 +25,7 @@ public class Pawn extends Chess {
         
         //changes board based on move
         board[move2[0]][move2[1]] = ' ';
-        if (isWhite = true)
-            board[move2[2]][move2[3]] = '♙';
+        if (isWhite == true) board[move2[2]][move2[3]] = '♙';
         else board[move2[2]][move2[3]] = '♟';
         return board;
         }
