b68128ee8561b702ae3926ce1640e0508c76d98d
Delilah Adams <159796184+delilahadams@users.noreply.github.com>
2024-05-06 13:02:02 -0400

    finished rook function and readme + general updates to rest of code

--- Chessmaxing/src/Chess.java
+++ Chessmaxing/src/Chess.java
@@ -3,13 +3,16 @@ public class Chess {
     public static void main(String[] args) throws Exception {
         {
         String[] options = {"Play", "Options", "Exit"};
-            int mainMenuOption = JOptionPane.showOptionDialog(null, "welcome to Chess", "Chessmaxxing", 0, 3, null, options, options[0]);
+        boolean gameOver = false;
+            int mainMenuOption = JOptionPane.showOptionDialog(null, "Welcome to Chess!", "Chessmaxxing", 0, 3, null, options, options[0]);
             if(mainMenuOption == 0);
                 Character[][] board = {{'♜','♞','♝','♚','♛','♞','♝','♜'}, {'♟', '♟', '♟','♟','♟','♟','♟','♟',}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' }, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' }, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' }, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' }, {'♙', '♙', '♙', '♙', '♙', '♙', '♙', '♙'}, {'♖', '♘', '♗', '♕', '♔', '♗', '♘', '♖' }};
-                int turnNumber = 1;
-                String boardDisplay = PrintBoard.main(board);
+                int turnNumber = 0;
+            while (gameOver == false)
+                {String boardDisplay = PrintBoard.main(board);
                 String move = JOptionPane.showInputDialog(null, boardDisplay, "board", 1);
                 board = Engine.main(move, board, turnNumber);
+                }
         };
     }
 }
